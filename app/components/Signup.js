4ca9e4e99dcb5d44ea98dc20fa935b69b72930c3
ewaight12 <ewaight12@gmail.com>
2017-05-02 10:53:31 -0400

    Initial Branch Test and added Signup Component and enclosing folder

---
+++ app/components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  AppRegistry,
+  Text,
+  Button,
+  View,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class Signup extends React.Component {
+    
+}
