72fbc64cc0aac52bfb3a0147cc4a453d09571cd5
Sarah Peterson <sarahpeterson@Sarahs-MacBook-Pro.local>
2017-05-04 13:36:14 -0400

    added style file

---
+++ app/style.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import {
+  StyleSheet,
+} from 'react-native';
+
+
+const styles = StyleSheet.create ({
+default: {
+  fontSize: 20,
+  color: '#FFFFFF',
+  //backgroundColor: '#373435',
+},
+  View: {
+    flex: 1,
+    //flexGrow: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    textAlign: 'center',
+    alignItems: 'flex-end',
+    //position: 'absolute',
+    //bottom: 0,
+    backgroundColor: '#373435',
+  },
+  Pic: {
+    flex: 1,
+    //flexDirection: 'column',
+    justifyContent: 'center',
+    //textAlign: 'center',
+    alignItems: 'center',
+    //position: 'absolute',
+    //bottom: 0,
+    backgroundColor: '#373435',
+  },
+
+  Pic2: {
+    //flex: 1,
+    //flexDirection: 'column',
+    justifyContent: 'center',
+    //textAlign: 'center',
+    alignItems: 'flex-start',
+    //position: 'absolute',
+    //bottom: 0,
+    //backgroundColor: '#373435',
+  },
+  SignUp: {
+    flex: 1,
+    //alignItems: 'flex-end',
+    // fontFamily: 'Avenir, sans-serif',
+    //fontSize: 16,
+    //width: 200,
+    //lineHeight: 22,
+    //textAlign: 'center',
+    color: '#FFFFFF',
+    backgroundColor: '#FF2BDC',
+  },
+  LogIn: {
+    flex: 1,
+    //alignItems: 'flex-end',
+    // fontFamily: 'Avenir, sans-serif',
+    //fontSize: 16,
+    //width: 200,
+    //lineHeight: 22,
+    //textAlign: 'center',
+    color: 'white',
+    backgroundColor: '#00CCFF',
+  },
+  // TextField: {
+  //   // fontFamily: 'Avenir, sans-serif',
+  //   fontSize: 30,
+  //   lineHeight: 19,
+  //   color: '#D6D6D6',
+  // },
+  // Prompt: {
+  //   // fontFamily: 'DINCondensedBold, sans-serif',
+  //   fontWeight: 'bold',
+  //   fontSize: 30,
+  //   lineHeight: 30,
+  //   color: '#FFFFFF',
+  // },
+  container: {
+      flex: 1,
+      //flexDirection: 'column',
+      //justifyContent: 'space-between',
+      //textAlign: 'center',
+      //alignItems: 'flex-start',
+    justifyContent:'center',
+  //    paddingTop: 200,
+  //    fontSize: 16,
+  //    lineHeight: 30,
+        //color: 'white',
+      backgroundColor: '#373435',
+      color: '#D6D6D6',
+  },
+  input: {
+    //flex: 0,
+    //alignItems: 'flex-start',
+    //justifyContent: 'left',
+    //flexDirection: 'row',
+    //  margin: 15,
+     height: 40,
+     //borderColor: 'grey',
+     //borderWidth: 1,
+     fontWeight: 'bold',
+     fontSize: 16,
+    //  lineHeight: 30,
+     color: '#D6D6D6',
+     textAlign: 'left',
+  },
+  swipe: {
+    //flex: 1,
+    textAlign: 'center',
+    color: 'white',
+    alignItems: 'flex-start',
+  },
+});
+
+
+export default styles;
