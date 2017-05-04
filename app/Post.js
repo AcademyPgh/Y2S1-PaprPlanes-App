7726239f46a246e42abe2cdbefc53dd4939ec142
Rabia Almakhoul <moonroor@Rabias-MacBook-Pro.local>
2017-05-04 15:48:59 -0400

    login functionality

--- app/Post.js
+++ app/Post.js
@@ -9,10 +9,27 @@ import {
 } from 'react-native';
 
 class Post extends React.Component {
+  constructor(props){
+   super(props);
+   this.showIcon = this.showIcon.bind(this);
+   }
+
+
+  showIcon()
+  { if (this.props.information.Type === 0)
+     {return "this is picture";}
+    else if (this.props.information.Type === 1) {
+      return "this is Text";
+    }
+    else {return "this is URL";}
+  }
+
+
+
   render(){
     return (
         <View >
-               <Text>{this.props.information.Type}</Text>
+               <Text>{this.showIcon()}</Text>
                 <Text>{this.props.information.AuthorName}</Text>
                 <Text>{this.props.information.SenderName}</Text>
                 <Text>-----------------------------</Text>
