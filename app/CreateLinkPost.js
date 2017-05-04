807c24b5a25c717264c48092f2513ae611cb4a28
Michael <michaelwkaiser@gmail.com>
2017-05-04 10:07:18 -0400

    added files

---
+++ app/CreateLinkPost.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  AppRegistry,
+  Text,
+  Button,
+  View,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import { StackNavigator } from 'react-navigation';
+
+class CreateLinkPost extends React.Component {
+  render(){
+    return (
+        <View>
+        <Text>LINK POST</Text>
+        </View>
+    );
+  }
+
+}
+
+export default CreateLinkPost;
---
+++ app/CreatePicPost.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  AppRegistry,
+  Text,
+  Button,
+  View,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import { StackNavigator } from 'react-navigation';
+
+class CreatePicPost extends React.Component {
+  render(){
+    return (
+        <View>
+        <Text>PIC POST</Text>
+        </View>
+    );
+  }
+
+}
+
+export default CreatePicPost;
