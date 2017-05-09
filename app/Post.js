5b30daaad80539718c087558168e2a6e24c91949
Jason Johnson <johnson.jason.a@gmail.com>
2017-05-09 13:59:32 -0400

    Modal added

--- app/Post.js
+++ app/Post.js
@@ -7,14 +7,17 @@ import {
   TextInput,
   StyleSheet,
   Image,
+  TouchableHighlight,
 } from 'react-native';
 import style from './style';
 import CreateTextPost2 from '../Resources/createtextpost2.png';
 import camera2 from '../Resources/camera2.png';
 import link2 from '../Resources/link2.png';
 
+import ModalDropdown from 'react-native-modal-dropdown';
 
 class Post extends React.Component {
+
   constructor(props){
    super(props);
 
@@ -36,16 +39,28 @@ chooseIcon() {
 }
 
   render(){
+    const navigate = this.props.navigate;
     return (
+<View>
+
+  <ModalDropdown options={[this.props.information.Sendername,
+    this.props.information.AuthorName,
+    this.props.information.Caption,
+    this.props.information.Content]}>
         <View style = {style.PostLinkBoxMain}>
-               <View style = {style.PostLinkBoxSub}>
-               <Text style = {style.SenderName}>{this.props.information.SenderName}</Text>
-                <Text style = {style.CreatorName}>{this.props.information.AuthorName}</Text>
-                </View>
-                <View style = {style.PostIcon}>
-                <Image source = {this.chooseIcon()}/>
-                </View>
+
+              <View style = {style.PostLinkBoxSub}>
+                 <Text style = {style.SenderName}>{this.props.information.SenderName}</Text>
+                 <Text style = {style.CreatorName}>{this.props.information.AuthorName}</Text>
+              </View>
+              <View style = {style.PostIcon}>
+                  <Image source = {this.chooseIcon()}/>
+              </View>
        </View>
+       {/* <SeePost information={this.props.information} navigate={navigate} /> */}
+</ModalDropdown>
+</View>
+
     );
   }
 
